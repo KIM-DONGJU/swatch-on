7b95d0890822ac0c640fadb27bf3af9ea23ecec5
KIM-DONGJU <kdj0689@gmail.com>
2022-08-09 23:13:35 +0900

    mood-board-item 일부 스타일링

--- components/MoodBoardIThreeItems.vue
+++ components/MoodBoardIThreeItems.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="mood-board-three-items-container">
     <MoodBoardItem
-      v-for="item in bundleMoodBoardItems"
-      :key="getMoodBoardItemKey(item)"
+      v-for="moodBoardItem in bundleMoodBoardItems"
+      :key="getMoodBoardItemKey(moodBoardItem)"
+      :moodBoardItemModelImages="getMoodBoardItemModelImagesMaxFour(moodBoardItem)"
     />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { marketingItemType } from '@/interface/marketingItems';
+import { marketingItemsImageType, marketingItemType } from '@/interface/marketingItems';
 import { MoodBoardItem } from '@/components';
 
 
@@ -16,15 +17,42 @@ const props = defineProps<{
   bundleMoodBoardItems: marketingItemType[],
 }>();
 
-const getMoodBoardItemKey = (item: marketingItemType) => {
-  const itemId = item.id;
+const getMoodBoardItemKey = (moodBoardItem: marketingItemType): string => {
+  const itemId = moodBoardItem.id;
   const moodBoardItemKey = `marketing-items-key-${itemId}`;
 
   return moodBoardItemKey
 }
 
+const getMoodBoardItemModelImagesMaxFour = (moodBoardItem: marketingItemType): marketingItemsImageType[]  => {
+  const moodBoardItemModelImages = moodBoardItem.images || [];
+  const isModelImageOverFour = moodBoardItemModelImages.length > 4;
+
+  if (isModelImageOverFour) {
+    const moodBoardItemModelImagesMaxFour = moodBoardItemModelImages.slice(0, 4);
+
+    return moodBoardItemModelImagesMaxFour;
+  };
+
+  return moodBoardItemModelImages;
+}
+
 </script>
 
 <style lang="scss" scoped>
+.mood-board-three-items-container {
+  width: 100%;
+  max-width: 1920px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16px;
+  align-items: center;
+}
 
+@media screen and (min-width: 1200px) {
+  .mood-board-three-items-container {
+    padding: 0 48px;
+  }
+}
 </style>
--- components/MoodBoardItem.vue
+++ components/MoodBoardItem.vue
@@ -1,13 +1,61 @@
 <template>
   <div class="mood-board-item-container">
-    example
+    <div class="mood-board-item-model-images">
+      <div
+        class="wrap-model-image"
+        v-for="(modelImage, index) in moodBoardItemModelImages"
+        :key="index"
+      >
+        <img
+          class="model-image"
+          :src="modelImage.large"
+          alt="SwatchOn - No. 1 Wholesale fabric platform for Fashion Brands"
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { marketingItemsImageType } from '@/interface/marketingItems';
+
+const props = defineProps<{
+  moodBoardItemModelImages: marketingItemsImageType[]
+}>();
 
 </script>
 
 <style lang="scss" scoped>
+.mood-board-item-container {
+  width: 100%;
+  height: 100%;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 3px 6px rgb(0 0 0 / 10%);
+  overflow: hidden;
+  -webkit-backface-visibility: hidden;
+  -moz-backface-visibility: hidden;
+  -webkit-transform: translateZ(0);
+  -moz-transform: translateZ(0);
 
+  .mood-board-item-model-images {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+    .wrap-model-image {
+      flex: 1;
+      height: 100%;
+
+      .model-image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+  }
+
+}
 </style>
